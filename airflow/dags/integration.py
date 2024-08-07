9aedfcbf02b01a6a618c04b8ef8407ce35f62ef0
Sophia Lawal <lawalsophia@gmail.com>
2024-08-07 07:47:22 +0200

    feat: use taskflow API

--- airflow/dags/integration.py
+++ airflow/dags/integration.py
@@ -4,6 +4,10 @@ from airflow.models.dag import DAG
 from airflow.operators.empty import EmptyOperator
 from airflow.providers.common.sql.operators.sql import SQLExecuteQueryOperator
 from airflow.utils.task_group import TaskGroup
+from airflow.decorators import  task
+from airflow.providers.postgres.hooks.postgres import PostgresHook
+from airflow.providers.common.sql.hooks.sql import  fetch_all_handler
+from airflow.providers.google.cloud.hooks.bigquery import BigQueryHook
 
 DAG_ID = "integration"
 with DAG(
@@ -12,33 +16,32 @@ with DAG(
     schedule="@once",
     catchup=False,
 ) as dag:
-    with TaskGroup("extract_from_postgres", tooltip="Tasks for extracting data from postgres") as postgres:
+    with TaskGroup("extract") as extract:
+        extract_tasks = []
+        for  table_name in ["customers", "sellers", "orders"]:
+            @task(multiple_outputs=True, task_id=f"fetch_data_{table_name}")
+            def fetch_data(table_name: str = "customers", postgres_conn_id: str = "postgres_conn_id",):
+                pg_hook = PostgresHook(postgres_conn_id=postgres_conn_id)
+                results = pg_hook.run(
+                    handler = fetch_all_handler,
+                    parameters = [],
+                    sql=f" SELECT * FROM olist.{table_name} limit 3;",
+                )    
+                return {table_name: results}
+            extract_tasks.append(fetch_data(table_name=table_name))
+    
+    
+    
+    
+    def load_data(extract_tasks):
+        bq_hook = BigQueryHook(gcp_conn_id="gcp_conn_id")
+        bq_hook.create_empty_table()
+        client = bq_hook.get_client()
+        
+    
+    
+    
+    #fetch_data(table_name="customers", postgres_conn_id= "postgres_conn_id")
 
-        extract_query = [
-            SQLExecuteQueryOperator(
-                conn_id="postgres_conn_id",
-                task_id=f"extract_{table_name}",
-                parameters=[100],
-                sql=f" SELECT * FROM olist.{table_name} LIMIT 3;",
-                split_statements=True,
-                return_last=False,
-            )
-            for table_name in [
-                "geolocation",
-                "customers",
-                "sellers",
-                "orders",
-                "product_category_name_translation",
-                "products",
-                "order_payments",
-                "order_reviews",
-                "order_items",
-            ]
-        ]
 
 
-    with TaskGroup("load_to_bigquery", tooltip="Tasks for loading data into bigquery") as data_warehouse:
-        load = EmptyOperator(task_id="start")
-
-    extract_query >> load
-    postgres >> data_warehouse
