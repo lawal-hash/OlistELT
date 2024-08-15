8a2a43a45be9fad8d4b1e7a318e50dc1cf316610
Sophia Lawal <lawalsophia@gmail.com>
2024-08-15 04:43:12 +0200

    fix: full name of state code

--- dbt/olist_dbt/models/intermediate/int_orders_by_state.sql
+++ dbt/olist_dbt/models/intermediate/int_orders_by_state.sql
@@ -7,8 +7,15 @@ with order_by_state as (
     join {{ ref('stg_customers')}} c
     using (customer_id)
 
-)
+), count_order_by_state as (
 select customer_state,
 count(order_id) as total_orders,
 from order_by_state
-GROUP BY customer_state
+GROUP BY customer_state)
+
+select 
+    s.state as customer_state,
+    cs.total_orders 
+from count_order_by_state cs
+join {{ref('states')}} s
+on cs.customer_state = s.code
