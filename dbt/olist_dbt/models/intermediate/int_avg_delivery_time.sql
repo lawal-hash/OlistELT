9309b5e9dce85a7088e132603e85bf020ba81b35
Sophia Lawal <lawalsophia@gmail.com>
2024-08-15 04:42:05 +0200

    fix: improve day calculation

--- dbt/olist_dbt/models/intermediate/int_avg_delivery_time.sql
+++ dbt/olist_dbt/models/intermediate/int_avg_delivery_time.sql
@@ -1,12 +1,12 @@
 
-with order_delivery  as (
+with order_delivery_hours  as (
     select o.order_id,
-    DATETIME_DIFF(o.order_delivered_customer_date, o.order_purchase_timestamp, DAY) as delivery_time,
+    DATETIME_DIFF(o.order_delivered_customer_date, o.order_purchase_timestamp, HOUR) as delivery_time,
     from {{ref('stg_orders')}} o
 
 
 )
 select order_id,
-avg(delivery_time) as avg_delivery_time_per_order
-from order_delivery
+avg(delivery_time)/24 as avg_delivery_time_per_order
+from order_delivery_hours
 GROUP BY order_id
