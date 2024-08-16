b1e519b2651347cb58c1afd5d20ea17db7984088
Sophia Lawal <lawalsophia@gmail.com>
2024-08-16 12:43:28 +0200

    fix: rename file

--- dbt/olist_dbt/models/marts/fct_sales_by_category.sql
+++ dbt/olist_dbt/models/marts/fct_sales_by_category.sql
@@ -1,6 +1,6 @@
 with sales  as (
     select 
-    product_category_name,
+    product_category,
     total_sales,
     rank() over (order by total_sales desc) as category_rank
     from {{ref('int_sales_by_category')}} 
@@ -8,7 +8,7 @@ with sales  as (
 
 )
 
-select product_category_name, total_sales
+select product_category, total_sales
 from sales
 where category_rank = 1
 
